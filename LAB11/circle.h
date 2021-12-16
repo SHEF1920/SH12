d38a2ebc287e79c92a3150a7aa1548fb3308518c
SHEF1920 <90445792+SHEF1920@users.noreply.github.com>
2021-12-16 18:52:39 +0300

    Create circle.h

---
+++ LAB11/circle.h
@@ -0,0 +1,10 @@
+#ifndef LAB11_CIRCLE_H_INCLUDED
+#define LAB11_CIRCLE_H_INCLUDED
+
+#include "point.h"//используем файл point.h
+struct Circle//структура круга
+{
+    struct point O;//центр
+    struct point M;//точка на окружности
+};
+#endif
